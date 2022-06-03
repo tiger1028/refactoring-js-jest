fbf4317a523a0083fb23e5f20d43b87dd4e12593
tiger1028 <gentleman.tiger1028@gmail.com>
2022-06-03 14:12:22 -0700

    feat: prepare all test cases

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,121 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+describe("Gilded Rose - Normal Items", function () {
+  it("normal day out", function () {
+    const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(19);
+  });
+
+  it("normal day out with expired sellIn", function () {
+    const gildedRose = new Shop([new Item("Elixir of the Mongoose", 0, 7)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(5);
+  });
+
+  it("normal day out with expired sellIn and minimum quality", function () {
+    const gildedRose = new Shop([new Item("Elixir of the Mongoose", -1, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-2);
+    expect(items[0].quality).toBe(0);
+  });
+});
+
+describe("Gilded Rose - Aged Brie", function () {
+  it("normal day out", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 2, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(1);
+    expect(items[0].quality).toBe(1);
+  });
+
+  it("normal day out with maximum quality", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 2, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(1);
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("normal day out with expired sellIn", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 43)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(45);
+  });
+});
+
+describe("Gilded Rose - Sulfuras", function () {
+  it("normal day out", function () {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(0);
+    expect(items[0].quality).toBe(80);
+  });
+
+  it("normal day out with expired sellIn", function () {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(80);
+  });
+});
+
+describe("Gilded Rose - Backstage passes", function () {
+  it("normal day out", function () {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(14);
+    expect(items[0].quality).toBe(21);
+  });
+
+  it("normal day out within 10 days", function () {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("normal day out within 5 days", function () {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(4);
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("normal day out with expired sellIn", function () {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(0);
+  });
+});
+
+describe("Gilded Rose - Conjured", function () {
+  it("normal day out", function () {
+    const gildedRose = new Shop([new Item("Conjured Mana Cake", 3, 6)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(2);
+    expect(items[0].quality).toBe(4);
+  });
+
+  it("normal day out with expired sellIn", function () {
+    const gildedRose = new Shop([new Item("Conjured Mana Cake", 0, 6)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(2);
   });
 });
